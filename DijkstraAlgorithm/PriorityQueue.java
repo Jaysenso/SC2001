bd9ade96f20d150742cf2d34c47ebb02d7e2de16
Jaysenso <jsenongxuan@gmail.com>
2023-10-24 12:44:30 +0800

    Minor edits

--- DijkstraAlgorithm/PriorityQueue.java
+++ DijkstraAlgorithm/PriorityQueue.java
@@ -4,31 +4,24 @@ import java.util.*;
 public class PriorityQueue {
     
     //array will be our dataStructure of choice to implement priority queue
-    //this array will store datatype "node" that stores the name of the node & the distance to
+    //this array will store datatype "Edge" that stores the name of the node & the distance to
     //source node. This will act as a priority queue as well as a data structure to store/update shortest distance
-    private ArrayList<Edge> queue;
-
+    private final ArrayList<Edge> queue;
 
     //constructor
     public PriorityQueue(){
         this.queue = new ArrayList<Edge>();
     }
 
-    //PriorityQueue functions
-    public void enqueue(int node, int distance){
-        Edge edge = new Edge(node, distance);
-        queue.add(edge);
-    }
-
     public Edge extractCheapest(){
-        if(queue.isEmpty()){
+        if(queue.isEmpty())
+            return null;
 
-        }
-        Edge cheapestEdge = null;
         int cheapestIndex = -1; //act as a flag to signal that we found the cheapestEdge and for deletion/dequeue
         int distance = Integer.MAX_VALUE;
 
         //we iterate through the queue looking for the smallest distance
+        Edge cheapestEdge = null;
         for(int i = 0; i < queue.size(); i++){
             //if we found a cheaper distance cost, we update cheapestEdge
             if(queue.get(i).getDistance() < distance){
@@ -44,9 +37,13 @@ public class PriorityQueue {
         return cheapestEdge;
     }
 
+    //PriorityQueue functions
+    public void enqueue(int node, int distance){
+        queue.add(new Edge(node, distance));
+    }
 
     public boolean isEmpty(){
-        return queue.size() == 0;
+        return queue.isEmpty();
     }
 
 
@@ -60,21 +57,20 @@ public class PriorityQueue {
         Edge edge = null;
         
         System.out.println();
-        System.out.printf("To Node:\t|\t");
-        for(int i = 0; i < queue.size(); i++){
-            edge = queue.get(i);
-            System.out.printf("%d\t",edge.getNode() + 1);
+        System.out.print("To Node:\t|\t");
+        for (Edge value : queue) {
+            edge = value;
+            System.out.printf("%d\t", edge.getNode() + 1);
         } 
         System.out.println();
 
-        System.out.printf("Distance:\t|\t");
-        for(int i = 0; i < queue.size(); i++){
-            edge = queue.get(i);
-            if(edge.getDistance() == Integer.MAX_VALUE){
+        System.out.print("Distance:\t|\t");
+        for (Edge value : queue) {
+            edge = value;
+            if (edge.getDistance() == Integer.MAX_VALUE) {
                 char inf = '\u221E';
-                System.out.printf("%c\t",inf);
-            }
-            else
+                System.out.printf("%c\t", inf);
+            } else
                 System.out.printf("%d\t", edge.getDistance());
         }
         System.out.println();
